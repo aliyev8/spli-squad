9406c9c98ab00e77de4ff1b807ec28c427294842
Allahyar <81466577+aliyev8@users.noreply.github.com>
2024-08-01 20:03:43 +0400

    Fixed: teams

--- src/components/Teams.jsx
+++ src/components/Teams.jsx
@@ -2,26 +2,26 @@
 export default function Teams({teams}) {
     return <div className='flex gap-8 mt-8 justify-center'>
 
-        <ul>
+        {teams && teams[0] && teams[0].length > 0 && <ul>
             <h4 className='text-lg font-bold'>Ev komandası</h4>
             {
-                teams && teams[0] && teams[0].length > 0 && teams[0].map(({name,id}) => (
+                teams[0].map(({name, id}) => (
                     <li key={id}>
                         {name}
                     </li>
                 ))
             }
         </ul>
-
-        <ul>
+        }
+        {teams && teams[1] && teams[1].length > 0 && <ul>
             <h4 className='text-lg font-bold'>Səfər komandası</h4>
             {
-                teams && teams[1] && teams[1].length > 0 && teams[1].map(({name,id}) => (
+                teams[1].map(({name, id}) => (
                     <li key={id}>
                         {name}
                     </li>
                 ))
             }
-        </ul>
+        </ul>}
     </div>
 }
